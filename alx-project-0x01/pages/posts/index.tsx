caebf721e4b1b4e8d7d4a07fb119e5b346ac67e1
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-07-21 11:31:15 +0200

    feat: implement user management

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,8 @@
 import PostCard from "@/components/common/PostCard";
 import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostData, PostProps, PostsPageProps } from "@/interfaces";
+import { PostData, PostsPageProps } from "@/interfaces";
+import { PostProps } from "@/interfaces";
 import { useState } from "react";
 
 const Posts: React.FC<PostsPageProps> = ({ posts }) => {
